6f8da8466ed55e563fffa2643cfbd1e28d0d4862
jayanthimuralirajan <j.jayanthi42@gmail.com>
2025-07-31 19:39:03 +0530

    changed the apiurl

--- task-management-frontend/src/api/api.js
+++ task-management-frontend/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://127.0.0.1:8000/'; // Adjust based on your Django backend URL
+const API_URL = 'https://projectmanagement-tyik.onrender.com'; // Adjust based on your Django backend URL
 
 export const registerUser = (userData) => axios.post(`${API_URL}register/`, userData);
 export const loginUser = (credentials) => axios.post(`${API_URL}userlogins/`, credentials);
